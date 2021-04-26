9b1e7a9ca5bb6766e503fcc2772469a2c5e7904c
Nam <namplt071099@gmail.com>
2021-04-26 11:42:47 +0700

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Nam on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
